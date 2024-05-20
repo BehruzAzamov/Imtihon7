ff3065c71d814dc12d4a0fb17439f89931f124e1
azamovbehruz <azamovbehruz@gmail.com>
2024-05-20 08:02:15 +0500

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
-import HomePage from './components/HomePage';
-import Nagivation from './components/Navigation';
-import Footer from './components/Footer';
-import HeadPhones from './components/HeadPhones';
-import Speakers from './components/Speakers';
-import EarPhones from './components/EarPhones';
-import ProductDetail from './components/ProductDetail';
-import Checkout from './components/Checkout';
-import Thankyou from './components/Thankyou';
+import {
+  HomePage,
+  Nagivation,
+  Footer,
+  HeadPhones,
+  Speakers,
+  EarPhones,
+  ProductDetail,
+  Checkout,
+  Thankyou,
+} from './components/'
 
 function App() {
-  const [product, setProduct] = useState({});
-  const [data, setDate] = useState([]);
+    const [product, setProduct] = useState({});
+    const [data, setDate] = useState([]);
 
-  useEffect(() => {
-    fetch("./data.json")
-      .then(response => response.json())
-      .then(value => setDate(value))
-  }, [])
+    useEffect(() => {
+      fetch("./data.json")
+        .then(response => response.json())
+        .then(value => setDate(value))
+    }, [])
 
-  function viewProduct(slug) {
-    data.map(item => item.slug == slug && setProduct(item))
+    function viewProduct(slug) {
+      data.map(item => item.slug == slug && setProduct(item))
+    }
+
+    return (
+      <Router>
+        <div style={{ backgroundColor: "#141414" }}>
+          <Nagivation />
+        </div>
+        <Routes>
+          <Route exact path="/" element={<HomePage send={slug => viewProduct(slug)} />} />
+          <Route exact path="/headphones" element={<HeadPhones send={slug => viewProduct(slug)} />} />
+          <Route exact path="/speakers" element={<Speakers send={slug => viewProduct(slug)} />} />
+          <Route exact path="/earphones" element={<EarPhones send={slug => viewProduct(slug)} />} />
+          <Route exact path="/prdDetail" element={<ProductDetail product={product} send={slug => viewProduct(slug)} />} />
+          <Route exact path="/checkout" element={
+            <>
+              <Checkout />
+              <Thankyou />
+            </>
+          } />
+        </Routes>
+        <Footer />
+      </Router>
+    );
   }
-
-  return (
-    <Router>
-      <div style={{ backgroundColor: "#141414" }}>
-        <Nagivation />
-      </div>
-      <Routes>
-        <Route exact path="/" element={<HomePage send={slug => viewProduct(slug)} />} />
-        <Route exact path="/headphones" element={<HeadPhones send={slug => viewProduct(slug)} />} />
-        <Route exact path="/speakers" element={<Speakers send={slug => viewProduct(slug)} />} />
-        <Route exact path="/earphones" element={<EarPhones send={slug => viewProduct(slug)} />} />
-        <Route exact path="/prdDetail" element={<ProductDetail product={product} send={slug => viewProduct(slug)} />} />
-        <Route exact path="/checkout" element={
-          <>
-            <Checkout />
-            <Thankyou />
-          </>
-        } />
-      </Routes>
-      <Footer />
-    </Router>
-  );
-}
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export { default as HomePage } from "./HomePage";
+export { default as Nagivation } from "./Navigation";
+export {default as Footer} from './Footer'
+export {default as HeadPhones } from './HeadPhones'
+export {default as Speakers } from './Speakers'
+export {default as EarPhones} from './EarPhones'
+export {default as ProductDetail} from './ProductDetail'
+export {default as Checkout} from './Checkout'
+export {default as Thankyou} from './Thankyou'
